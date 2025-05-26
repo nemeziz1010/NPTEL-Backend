bdf737eceeba04ef6400acfd99c7b724c10a6a61
nemeziz1010 <mitukorochikar10@gmail.com>
2025-05-26 23:37:20 +0530

    DB env edits

--- db/index.js
+++ db/index.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT, 10),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false  // Required for connecting to Render
+  }
 });
 
 module.exports = pool;
